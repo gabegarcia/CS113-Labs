63a6c945e50ee9fcf2df852d0e0902159b4b0b2a
gabegarcia <gmgarcia1@gmail.com>
2021-01-27 20:09:47 -0800

    Add JavaDoc for FizzBuzz.java

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -1,8 +1,11 @@
 package lab1;
 
 /*
+ * FizzBuzz.java: Write a program that prints the numbers from 1 to 100, but for multiples of 3 print “Fizz” instead of the number and for multiples of 5 print “Buzz”. For numbers that are 
+ * multiples of both three and five print “FizzBuzz”.
  * 
- * 
+ * @author	Gabe Garcia (gmgarcia1@gmail.com)
+ * @version	1
  */
 
 public class FizzBuzz {
